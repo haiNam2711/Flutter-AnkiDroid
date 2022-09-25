cb8e013a9854b0167dca43adb22dd9fbd1a7db9e
haiNam2711 <namcuong2711@gmail.com>
2022-09-25 18:52:48 +0700

    Add 2 Routes. One is main screen, one is screen containing deck.

---
+++ lib/SecondRoute.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class SecondRoute extends StatefulWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  State<SecondRoute> createState() => _SecondRouteState();
+}
+
+class _SecondRouteState extends State<SecondRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: Container(
+            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            child: Column(
+              children: const [
+                Text(
+                  'AnkiDroid',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '10 cards due.',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.undo,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.bookmark_add,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_vert,
+              ),
+            ),
+          ],
+        ),
+        body: Center(),
+      ),
+    );
+  }
+}
